65104379aa9fac67442a980aae90f8b47c3e3551
Bora YILDIRIM <bora38780@hotmail.com>
2025-07-28 09:59:06 +0300

    Add Slack sync notification

--- secrets/slack-secret.tf
+++ secrets/slack-secret.tf
@@ -5,5 +5,5 @@ resource "aws_secretsmanager_secret" "slack_webhook" {
 
 resource "aws_secretsmanager_secret_version" "slack_webhook_value" {
   secret_id     = aws_secretsmanager_secret.slack_webhook.id
-  secret_string = "https://hooks.slack.com/services/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"
+  secret_string = "<to-be-set-in-cluster>"
 }
